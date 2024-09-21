8918b228dd509b8b34c023b005402aa5aa991904
asepKargo <85653279+axeladelwyn@users.noreply.github.com>
2024-09-21 00:31:12 +0000

    Sat, Sep 21, 2024, 7:31 AM +07:00

--- lecture2/scores0.c
+++ lecture2/scores0.c
@@ -1,7 +1,7 @@
 // Averages three (hardcoded) numbers
 
 #include <stdio.h>
-
+#include <cs50.h>
 int main(void)
 {
     // Scores
@@ -10,5 +10,18 @@ int main(void)
     int score3 = 33;
 
     // Print average
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    printf("Average: %f\n", average());
+}
+
+// make a function that return a float and average
+float average(int length, int array[])
+{
+    int sum = 0;
+    for(int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    // return float and average
+    return sum / (float) length;
+
 }
