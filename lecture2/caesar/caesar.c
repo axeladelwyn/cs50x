8941fa2e60f1d2935bddeb800988d05074cbcb2a
asepKargo <85653279+axeladelwyn@users.noreply.github.com>
2024-09-22 10:29:30 +0000

    Sun, Sep 22, 2024, 5:29 PM +07:00

--- lecture2/caesar/caesar.c
+++ lecture2/caesar/caesar.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
     if (argc != 2 )
     {
         printf("Missing/Too much argument\n");
+        return 1;
+    }
+    else if (!isdigit(argv[1][0]))
+    {
+        printf("Argument is not a digit\n");
         return 1;
     }
     printf("hello, %s\n", argv[1]);
